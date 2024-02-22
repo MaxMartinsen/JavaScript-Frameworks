e9916ec10d8c0fc1a676cdde4f2dd4646ab04d58
Max Martinsen <max.martinsen@hotmail.com>
2024-02-22 21:33:18 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import { useState } from 'react';
+
+
+function Product({ productName }) {
+  return (
+    <div>
+      {productName.type} 
+    </div>
+  )
+}
 
 function App() {
+  const productList = [
+    {
+      type: 'Melk'
+    },
+    {
+      type: 'Ost'
+    }
+    
+    ]
+  const [index, setIndex] = useState(0);
+  const hasNext = index < productList.length - 1;
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+  let productName = productList[index];
   return (
     <div className="App">
       <header className="App-header">
+      <div>
+      <Product productName = {productName}/>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      </div>
       </header>
     </div>
   );
