7ae791d2a749175733876d220785868ad3db50c2
Max Martinsen <max.martinsen@hotmail.com>
2024-02-22 20:35:21 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+  function onButtonClick() {
+    console.log('Button clicked!');
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+      <button onClick={onButtonClick}>Click me</button>
+    </div>
       </header>
     </div>
   );
