6f0a51910c70565605c75af45e5a49bc3d7047ad
Max Martinsen <max.martinsen@hotmail.com>
2024-02-22 20:16:44 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+function Person(props) {
+  return (
+    <div className='flex'>
+      <p>First name: {props.firstName}</p>
+      <p>Last name: {props.lastName}</p>
+      <p>City: {props.city}</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Person firstName = 'Max' lastName = 'Martinsen' city = 'KRS'/>
+        <Person firstName = 'Ket' lastName = 'Martinsen' city = 'SVG'/>
       </header>
     </div>
   );
