a2525a22b9dcb64544584f7033f85772337df6b6
Max Martinsen <max.martinsen@hotmail.com>
2024-02-24 09:48:41 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
 
+function ProductList({productsMap}) {
+  return (
+    <div>
+    {productsMap.map((product) => (
+      <div key={product.id}>
+        <h2>{product.title}</h2>
+        <p>${product.price}</p>
+      </div>
+    ))}
+  </div>
+  )
+}
+
 function App() {
+
+  const products = [
+    { id: 0, title: 'Bread', price: 19.99 },
+    { id: 1, title: 'Milk', price: 29.99 },
+    { id: 2, title: 'Cheese', price: 35.99 },
+    { id: 3, title: 'Water', price: 15.99 },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
+        <ProductList productsMap = {products}/>
       </header>
     </div>
   );
