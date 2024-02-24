30051b05bb4eb3e98a1f12bde648d3a8e9b3aa58
Max Martinsen <max.martinsen@hotmail.com>
2024-02-24 08:47:55 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useState } from "react";
+
+function Product({title}) {
+  return (
+    <div>{title}</div>
+  )
+}
 
 function App() {
+
+  const [productTitle, setProductTitle] = useState('Milk')
+
+  function onButtonClick() {
+    setProductTitle('Cheese')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <Product title={productTitle}/>
+        <button onClick={onButtonClick}>Next</button>
       </header>
     </div>
   );
