a00e41da667912a7970fc2f1c475aadb7323d570
Max Martinsen <max.martinsen@hotmail.com>
2024-02-25 10:53:36 +0100

    Don’t use React Fragments if there is only one top-level parent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <React.Fragment>
+      <li>First item</li>
+      <li>Second item</li>
+    </React.Fragment>
       </header>
     </div>
   );
