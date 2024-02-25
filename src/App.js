7be5ff85fdb67eaf70c3347c0fcebad7430b5167
Max Martinsen <max.martinsen@hotmail.com>
2024-02-25 12:43:17 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className={styles.container}></div>
       </header>
     </div>
   );
