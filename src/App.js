1be3588025f08b35abc2db938ab22a2d3deb1dac
Max Martinsen <max.martinsen@hotmail.com>
2024-02-25 15:55:02 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import React from 'react';
+import * as Styles from './App.styles'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background: ${(props) => props.theme.color.primary};
+  border-radius: 6px;
+  border: 0;
+  cursor: pointer;
+  color: ${(props) => props.theme.color.secondary};
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Styles.BaseButton>Base button</Styles.BaseButton>
+      <Styles.PrimaryButton>Base button</Styles.PrimaryButton>
+      <div>
+        <Button>Click me</Button>
+      </div>
       </header>
     </div>
   );
