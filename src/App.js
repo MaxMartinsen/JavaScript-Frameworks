eaa723201a66bd96e4a9afbd90133452e355a281
Max Martinsen <max.martinsen@hotmail.com>
2024-02-25 13:12:09 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import './styles.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <ul>
+      <li>Primary</li>
+      <li>Secondary</li>
+    </ul>
       </header>
     </div>
   );
