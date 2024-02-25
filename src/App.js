74e3df096d7b1ac10f2c6b688860309f4c634f13
Max Martinsen <max.martinsen@hotmail.com>
2024-02-25 18:27:33 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isActive, setIsActive] = useState(true);
+
+  function onButtonClick() {
+    setIsActive(currentState => !currentState);
+  }
   return (
     <div className="App">
       <header className="App-header">
+      <button onClick={onButtonClick}>Toggle</button>
+      <div>{isActive ? <div>I am active</div> : <div>I am not active</div>}</div>
       </header>
     </div>
   );
