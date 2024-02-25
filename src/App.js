d9e26f685ec05a663d6060b33eb50d0c1f2556dd
Max Martinsen <max.martinsen@hotmail.com>
2024-02-25 16:48:33 +0100

    Level 1 process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Logo from './images/image-logo-example.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={Logo} alt="Noroff logo" />
       </header>
     </div>
   );
