c3a20d0bb704cd334f4b1b09fe176d70f264ae1c
Max Martinsen <max.martinsen@hotmail.com>
2024-03-02 06:41:48 +0100

    <Outlet> and creating a <Layout> component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,75 @@
+import React from "react"
+import { Routes, Route, Link ,Outlet } from "react-router-dom"
 import './App.css';
 
-function App() {
+function Home() {
+  return <div>Home</div>
+}
+
+function Products() {
+  return <div>Products</div>
+}
+
+function RouteNotFound() {
+  return <div>Page not found</div>
+}
+
+function Nav() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+// Our header component that gets used in our <Layout> component
+function Header() {
+  return (
+    <header>
+      <div>Header with Logo and nav</div>
+      <Nav />
+    </header>
+  )
+}
+
+// Our footer component that gets used in our <Layout> component
+function Footer() {
+  return <footer>Website footer</footer>
+}
+
+// The <Outlet> from react-router-dom displays any child routes, almost like
+// passing through "children" in a component
+function Layout() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <div className="wrapper">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
