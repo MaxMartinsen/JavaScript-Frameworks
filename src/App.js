9e1b69fa1ddfd150fcb68978be9096cca9db4482
Max Martinsen <max.martinsen@hotmail.com>
2024-03-04 07:07:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('useEffect has run');
+  }, [counter]); // No dependency array
+
+  function onIncrementCounter() {
+    setCounter(counter + 1);
+  }
+
+  function onDecrementCounter() {
+    setCounter(counter - 1);
+  }
+
+  console.log('Component has rendered');
   return (
     <div className="App">
       <header className="App-header">
+      <div>Counter: {counter}</div>
+      <button onClick={onIncrementCounter}>+</button>
+      <button onClick={onDecrementCounter}>-</button>
       </header>
     </div>
   );
